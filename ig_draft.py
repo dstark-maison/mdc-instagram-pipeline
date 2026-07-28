c3d41fd07dc85afef4a77f68a5a12032d080d254
Daniel Stark <ds@maisondecocon.com>
2026-07-28 12:51:00 +0200

    Temporary debug: print create_media_container response

--- ig_draft.py
+++ ig_draft.py
@@ -213,6 +213,7 @@ def create_media_container(ig_user_id, access_token, image_url, caption):
         f"{GRAPH_BASE}/{ig_user_id}/media",
         data={"image_url": image_url, "caption": caption, "access_token": access_token},
     )
+    print("DEBUG create_media_container response:", resp.status_code, resp.text)  # TEMPORARY
     resp.raise_for_status()
     return resp.json()["id"]
 
